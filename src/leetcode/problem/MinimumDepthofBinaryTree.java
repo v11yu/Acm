47dc59874b64c56bfde3a55edc111840d134f8d4
v11 <442629928@qq.com>
2014-10-11 17:27:37 +0800

    1
    1

--- src/leetcode/problem/MinimumDepthofBinaryTree.java
+++ src/leetcode/problem/MinimumDepthofBinaryTree.java
@@ -1,5 +1,30 @@
 package leetcode.problem;
 
-public class MinimumDepthofBinaryTree {
+import java.util.*;
 
+import leetcode.model.TreeNode;
+
+public class MinimumDepthofBinaryTree {
+	public int minDepth(TreeNode root) {
+		int ans = 0;
+		Queue<TreeNode> que = new LinkedList<TreeNode>();
+		if(root == null) return 0;
+		root.val = 1;
+		que.offer(root);
+		while(!que.isEmpty()){
+			TreeNode t = que.poll();
+			if(t.left == null && t.right ==null){
+				return t.val;
+			}
+			if(t.left!=null){
+				t.left.val = t.val + 1;
+				que.offer(t.left);
+			}
+			if(t.right!=null){
+				t.right.val = t.val + 1;
+				que.offer(t.right);
+			}
+		}
+		return ans;
+	}
 }
