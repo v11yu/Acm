841a7a45e343aae317d212c130ad2c39f529b037
v11 <442629928@qq.com>
2014-10-12 22:27:14 +0800

    1
    1

--- src/leetcode/problem/MaximumSubarray.java
+++ src/leetcode/problem/MaximumSubarray.java
@@ -1,5 +1,13 @@
 package leetcode.problem;
 
 public class MaximumSubarray {
-
+	public int maxSubArray(int[] A) {
+		int res = Integer.MIN_VALUE,tmp = 0;
+		for(int num:A){
+			tmp += num;
+			if(tmp>res) res = tmp;
+			if(tmp<0) tmp = 0;
+		}
+		return res;
+	}
 }
