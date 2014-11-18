3eff83dbcf855e5a5aebbee6f2c1a433282a712f
v11 <442629928@qq.com>
2014-11-18 16:35:23 +0800

    cpp D

--- src/codeforces/div_2_277/D.java
+++ src/codeforces/div_2_277/D.java
@@ -7,58 +7,61 @@ import java.util.*;
 import java.util.Map.Entry;
 
 public class D {
-	public static void main(String[] args) throws IOException {
-		
-		Map<String, Integer> map = new HashMap<String, Integer>();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-		int n = Integer.parseInt(tokenizer.nextToken());
-		int m = Integer.parseInt(tokenizer.nextToken());
-		ArrayList<Integer> edge[] = new ArrayList[n];
-		int c = 0;
-		int ex[] = new int[m];
-		int ey[] = new int[m];
-		for(int i=0;i<n;i++){
-			edge[i] = new ArrayList<Integer>();
-		}
-		for(int i =0;i<m;i++){
-			tokenizer = new StringTokenizer(reader.readLine());
-			int a = Integer.parseInt(tokenizer.nextToken());
-			int b = Integer.parseInt(tokenizer.nextToken());
-			a--;b--;
-			ex[i] = a ;
-			ey[i] = b;
-			if(!exist(a,b,map)){
-				edge[a].add(b);
-			}
-		}
-		map.clear();
-		for(int i=0;i<m;i++){
-			Integer from = ex[i];
-			for(Integer to:edge[ey[i]]){
-				//System.out.println(from +" "+ey[i]+" "+ to);
-				if(from == to) continue;
-				exist(from,to,map);
-			}
-		}
-		long ans = 0;
-		Iterator<Entry<String, Integer>> iter = map.entrySet().iterator();
-		while(iter.hasNext()){
-			Entry<String, Integer> en = iter.next();
-			//System.out.println(en.getKey()+" "+en.getValue());
-			long t = en.getValue();
-			ans += t*(t-1)/2;
-		}
-		System.out.println(ans);
+	/**
+	 * java will Tle, using cpp
+	 */
+	//============================================================================
+	// Name        : codeforces.cpp
+	// Author      : 
+	// Version     :
+	// Copyright   : Your copyright notice
+	// Description : Hello World in C++, Ansi-style
+	//============================================================================
+/*
+	#include <iostream>
+	#include <cstdio>
+	#include <vector>
+	#include <map>
+	using namespace std;
+	#define LL long long
+
+	const int M = 30000+5;
+	const int N = 3000+5;
+	int ex[M],ey[M];
+	map<string,int> mp;
+	vector<int> edge[N];
+	bool exist(int a,int b){
+	    string str = a+"#"+b;
 	}
-	public static boolean exist(int a,int b,Map<String, Integer> map){
-		String s = a+"#"+b;
-		if(map.containsKey(s)){
-			map.put(s, map.get(s)+1);
-			return true;
-		}else{
-			map.put(s, 1);
-			return false;
-		}
+	int mk[N][N];
+	int main() {
+	    int n,m;
+	    scanf("%d %d",&n,&m);
+	    for(int i=0;i<m;i++){
+	        scanf("%d %d",ex+i,ey+i);
+	        edge[ex[i]].push_back(ey[i]);
+	    }
+	    for(int i=0;i<m;i++){
+	        int from = ex[i];
+	        int k = ey[i];
+	        for(int j=0;j<edge[k].size();j++){
+	            int to = edge[k][j];
+	            if(from == to) continue;
+	            //printf("%d %d\n",from,to);
+	            mk[from][to]++;
+	        }
+	    }
+	    LL ans = 0;
+	    for(int i=1;i<=n;i++){
+	        for(int j=1;j<=n;j++){
+	            LL t = mk[i][j];
+	            //printf("%d %d %d\n",i,j,mk[i][j]);
+	            ans += t*(t-1) /2;
+	        }
+	    }
+	    //cout<<ans<<endl;
+	    printf("%I64d\n",ans);
+	    return 0;
 	}
+	*/
 }
