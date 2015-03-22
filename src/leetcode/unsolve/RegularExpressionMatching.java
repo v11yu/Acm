d5f2445777c098aa3b2b1c1294d71811d2fbf006
v11 <442629928@qq.com>
2015-03-22 09:53:08 +0800

    111
    111111

--- src/leetcode/unsolve/RegularExpressionMatching.java
+++ src/leetcode/unsolve/RegularExpressionMatching.java
@@ -1,59 +1,69 @@
 package leetcode.unsolve;
 
 public class RegularExpressionMatching {
-	boolean check1(int i,int j,String s,String p){
-		i--;j--;
-		if(s.charAt(i) == p.charAt(j)||s.charAt(i)=='.'||p.charAt(j)=='.'){
-			return true;
+
+	void display(String s,String p,boolean dp[][]){
+		int ls = s.length()+1;
+    	int lp = p.length()+1;
+		for(int i=0;i<ls;i++){
+			for(int j=0;j<lp;j++){
+				System.out.print(dp[i][j]+" ");
+			}
+			System.out.println();
 		}
-		return false;	
-	}
-	boolean check2(int i,int j,String s,String p){
-		if(i-1>=0)
-		if(s.charAt(i)=='*'&&s.charAt(i-1)==p.charAt(j)){
-			
-		}
-		return false;
-	}
-	boolean check3(int i,String s){
-		i--;
-		if(s.charAt(i)=='*') return true;
-		return false;
 	}
     public boolean isMatch(String s, String p) {
     	int ls = s.length()+1;
     	int lp = p.length()+1;
     	boolean dp[][] = new boolean[ls][lp];
-    
-    	System.out.println(dp[0][0]);
+
     	/*
     	 * init
     	 */
     	dp[0][0] = true;
-    	for(int i=2;i<ls;i++){
-    		if(check3(i,s)&&dp[i-2][0]) dp[i][0] = true;
+
+    	for(int i=2;i<lp;i++){
+    		int ii = i-1;
+    		if(p.charAt(ii)=='*'&&dp[0][i-2]) 
+    			dp[0][i] = true;
     	}
-    	for(int i=2;i<ls;i++){
-    		if(check3(i,p)&&dp[0][i-2]) dp[0][i] = true;
-    	}
+    	display(s,p,dp);
     	/*
     	 * dp
     	 */
     	for(int i=1;i<ls;i++){
-    		for(int j=1;j<=lp;j++){
+    		for(int j=1;j<lp;j++){
+    			if(i==3&&j ==5)
+    			{
+    				System.out.println();
+    			}
+    			int ii = i-1;
+				int jj = j-1;
     			if(dp[i-1][j-1]){
-    				if(check1(i,j,s,p))dp[i][j] = true;
-    				if(check2(i,j,s,p)) dp[i][j] = true;
-    				if(check2(j,i,p,s)) dp[i][j] = true;
+    				if(s.charAt(ii) == p.charAt(jj)||p.charAt(jj)=='.')dp[i][j] = true;
+    				if(p.charAt(jj)=='*'){
+    					if( p.charAt(jj-1)== s.charAt(ii) || p.charAt(jj-1) == '.')
+    						dp[i][j] = true;
+    				}
+    			}
+    			if(dp[i-1][j]){
+    				if(p.charAt(jj)=='*' ){
+    					if( p.charAt(jj-1)== s.charAt(ii) || p.charAt(jj-1) == '.')
+    						dp[i][j] = true;
+    				}
+    			}
+    			if(j-2>=0&&dp[i][j-2]){
+    				if(p.charAt(jj) == '*') dp[i][j] = true;
     			}
     		}
-    		
     	}
-		return false;
+    	System.out.println("=======");
+    	display(s,p,dp);
+		return dp[ls-1][lp-1];
     }
     public static void main(String[] args) {
-    	String s = "aab";
-    	String p = "c*a*b";
+    	String s = "aaba";
+    	String p = "ab*a*c*a";
 		System.out.println(new RegularExpressionMatching().isMatch(s, p));
 	}
 }
